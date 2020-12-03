6f9251468f0a765d2dec1bb59f95edfe331f5ed5
reuben21 <reuben211999@icloud.com>
2020-12-03 15:07:36 +0530

    Update with DTO

--- src/main/java/com/reubencoutinho/springrest/controller/MainController.java
+++ src/main/java/com/reubencoutinho/springrest/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.reubencoutinho.springrest.controller;
 
+import com.reubencoutinho.springrest.dto.CourseDto;
 import com.reubencoutinho.springrest.model.Course;
 import com.reubencoutinho.springrest.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MainController {
@@ -16,15 +19,30 @@ public class MainController {
 
 
     @GetMapping("/hello")
-    public String get_HelloW_World_Message(){
+    public String get_HelloW_World_Message() {
         return "Hello World";
     }
 
 
     @PostMapping("/api/course")
-    public Course postCourse(@RequestBody Course course)
-    {
+    public Course postCourse(@RequestBody Course course) {
         System.out.println(course.toString());
         return courseRepository.save(course);
     }
+
+    @GetMapping("/api/course")
+    public CourseDto getCourse() {
+        CourseDto dto = new CourseDto();
+        double total = 0;
+        List<Course> list = courseRepository.findAll();
+        for (Course c : list) {
+            total = total + c.getPrice();
+        }
+        total = total - (total * 0.1); //10% Discount
+        dto.setDiscountPrice(total);
+        dto.setList(list);
+        return dto;
+    }
+
+
 }
